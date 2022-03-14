97076353eaa354b683a86e77d4394d944e0bc173
Imane <imane.ourraoui175@gmail.com>
2022-03-14 15:04:35 +0100

    t

--- Package18155_18316.py
+++ Package18155_18316.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from IPython.display import display, clear_output
 
-def LeadLag_RT(MV,Kp,T,Ts,PV,PVInit=0,method='EBD'):
+def LeadLag_RT(MV, Kp, Tlead, Tlag, Ts, PV, PVinit=0, method='EBD'):
     
     """
     The function "LeadLag_RT" needs to be included in a "for or while loop".
@@ -24,18 +24,19 @@ def LeadLag_RT(MV,Kp,T,Ts,PV,PVInit=0,method='EBD'):
     The appended value is obtained from a recurrent equation that depends on the discretisation method.
     """    
     
-    if (T != 0):
-        K = Ts/T
+    if (Tlag != 0):
+        K = Ts/Tlag
         if len(PV) == 0:
-            PV.append(PVInit)
+            PV.append(PVinit)
         else: # MV[k+1] is MV[-1] and MV[k] is MV[-2]
             if method == 'EBD':
-                PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*MV[-1])
+                PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*((1+Tlead/Ts)*MV[0]-Tlead/Ts*PV[-1]))
             elif method == 'EFD':
-                PV.append((1-K)*PV[-1] + K*Kp*MV[-2])
-            elif method == 'TRAP':
-                PV.append((1/(2*T+Ts))*((2*T-Ts)*PV[-1] + Kp*Ts*(MV[-1] + MV[-2])))            
+                PV.append((1-K)*PV[-1] + K*Kp*(Tlead/Ts*MV[0]+(1-Tlead/Ts)*MV[-1]))
+            #elif method == 'TRAP':
+                #PV.append((1/(2*T+Ts))*((2*T-Ts)*PV[-1] + Kp*Ts*(MV[-1] + MV[-2])))            
             else:
-                PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*MV[-1])
+                #EBD par defaut
+                PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*((1+Tlead/Ts)*MV[0]-Tlead/Ts*PV[-1]))
     else:
         PV.append(Kp*MV[-1])
