c6ecb261f030d017659ab97b4a0fb2b9a1049a5c
Quentin JADOUL <18155@ecam.be>
2022-03-14 14:51:19 +0100

    leadLag

--- Package18155_18316.py
+++ Package18155_18316.py
@@ -1,59 +1,40 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import numpy as np\n",
-    "\n",
-    "import matplotlib.pyplot as plt\n",
-    "from IPython.display import display, clear_output\n",
-    "\n",
-    "#-----------------------------------\n",
-    "def lead_lag(MV, Kp, Tlead, Tlag, Ts, PV, PVinit=0, method='EBD'):\n",
-    "    \"\"\"\n",
-    "    The function \"lead_lag\" needs to be included in a \"for or while loop\".\n",
-    "    \n",
-    "    :MV: input vector\n",
-    "    :Kp: process gain\n",
-    "    :Tlead: lead time constant [s]\n",
-    "    :Tlag: lag time constant [s]\n",
-    "    :Ts: sampling period [s]\n",
-    "    :PV: output vector\n",
-    "    :PVInit: (optional: default value is 0)\n",
-    "    :method: discretisation method (optional: default value is 'EBD')\n",
-    "        EBD: Euler Backward difference\n",
-    "        EFD: Euler Forward difference\n",
-    "        TRAP: Trapezoïdal method\n",
-    "    \n",
-    "    The function \"lead_lag\" appends a value to the output vector \"PV\".\n",
-    "    The appended value is obtained from a recurrent equation that depends on the discretisation method.\n",
-    "    \"\"\"    \n",
-    "    "
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+import numpy as np
+
+import matplotlib.pyplot as plt
+from IPython.display import display, clear_output
+
+def LeadLag_RT(MV,Kp,T,Ts,PV,PVInit=0,method='EBD'):
+    
+    """
+    The function "LeadLag_RT" needs to be included in a "for or while loop".
+    
+    :MV: input vector
+    :Kp: process gain
+    :T: lag time constant [s]
+    :Ts: sampling period [s]
+    :PV: output vector
+    :PVInit: (optional: default value is 0)
+    :method: discretisation method (optional: default value is 'EBD')
+        EBD: Euler Backward difference
+        EFD: Euler Forward difference
+        TRAP: Trapezoïdal method
+    
+    The function "LeadLag_RT" appends a value to the output vector "PV".
+    The appended value is obtained from a recurrent equation that depends on the discretisation method.
+    """    
+    
+    if (T != 0):
+        K = Ts/T
+        if len(PV) == 0:
+            PV.append(PVInit)
+        else: # MV[k+1] is MV[-1] and MV[k] is MV[-2]
+            if method == 'EBD':
+                PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*MV[-1])
+            elif method == 'EFD':
+                PV.append((1-K)*PV[-1] + K*Kp*MV[-2])
+            elif method == 'TRAP':
+                PV.append((1/(2*T+Ts))*((2*T-Ts)*PV[-1] + Kp*Ts*(MV[-1] + MV[-2])))            
+            else:
+                PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*MV[-1])
+    else:
+        PV.append(Kp*MV[-1])
